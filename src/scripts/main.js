bb3c5971261b33981628530a6a90a567491b5689
Mário Henrique <marioh90@gmail.com>
2023-09-25 16:58:11 -0300

    aula 4: Criando a seção de atrações;
    ação de escolher aba ativa;

---
+++ src/scripts/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", (bt) => {
+            console.log(bt.target.dataset.tabButton); // nome de atributo 'data-tab-button'
+            const tabAttr = bt.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${tabAttr}]`);
+            
+            escondeAbas();
+            tab.classList.add('shows__list--is-active');
+        });
+    }
+})
+
+function escondeAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
