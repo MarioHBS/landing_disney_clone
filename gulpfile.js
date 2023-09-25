dbf931f2649c089d0c356d7bf646a3a521e01d45
Mário Henrique <marioh90@gmail.com>
2023-09-25 08:53:59 -0300

    pasta dist ignorada; teste de sass com gulp; pacotes gulp-rename, gulp-sass e sass intalados

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
-function testeGulp(cb) {
-    console.log("Olá, Gulp");
-    cb();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+const rename = require('gulp-rename');
+
+function styles(cb) {
+    return gulp.src('./src/styles/*.scss')
+        // .pipe(sourcemap.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(rename('style.css'))
+        // .pipe(sourcemap.write('./maps'))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = testeGulp;
+exports.default = styles;
