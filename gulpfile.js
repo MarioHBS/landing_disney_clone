0c838b2b0d2bc5cbc2a9ba29a6140963ee43e654
Mário Henrique <marioh90@gmail.com>
2023-09-27 17:04:52 -0300

    aula 10: Deploy
    adicionando mimificação do JS

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 
+const ugly = require('gulp-uglify');
+
 function styleSCSS() {
     return gulp.src('./src/styles/*.scss')
         // .pipe(sourcemap.init())
@@ -21,8 +23,16 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styleSCSS, images);
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(ugly())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+exports.default = gulp.parallel(styleSCSS, images, scripts);
+
 exports.watch = function() {
     // gulp.watch('.src/styles/*.scss', gulp.parallel(styleSCSS));
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.parallel(styleSCSS));
+    gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.parallel(scripts));
 }
