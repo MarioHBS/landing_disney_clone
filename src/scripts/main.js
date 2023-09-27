892f58af35b7e40b114f58fdb463905b3f149894
Mário Henrique <marioh90@gmail.com>
2023-09-27 15:14:42 -0300

    aula 10: criando o Cabeçalho

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const headerElmt = document.querySelector('.header');
+    const heightHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', () => {
+        const posicao = window.scrollY;
+        console.log('scroll: ' + posicao + ' | altura: ' + heightHero);
+        console.log(heroSection.classList);
+
+        if (posicao < heightHero) { // True: esconde os elementos
+            headerElmt.classList.add('header--is-hidden')
+        } else {
+            headerElmt.classList.remove('header--is-hidden')
+        }
+    })
+
+    // seção de atrações, programação da aba ativa
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", (bt) => {
             console.log(bt.target.dataset.tabButton); // nome de atributo 'data-tab-button'
@@ -15,10 +32,21 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    // seção FAQ, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', toggleQuestion)
     }
 })
+
+/* function hiddeElementsFromHeader() {
+    const header = document.querySelector('.header');
+    header.classList.
+} */
+
+function showElementsFromHeader() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden')
+}
 
 function toggleQuestion(elem) {
     const classe = 'faq__questions__item--is-open';
